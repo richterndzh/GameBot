8a66261feb21a1e0d8ba6a8c42fcdda49a32c1a9
richterndzh <richterndzh@gmail.com>
2022-02-08 11:26:57 +0300

    fix

--- script.js
+++ script.js
@@ -1,40 +1,74 @@
-'use strict';
+"use strict";
 
-const botNumber =  parseInt(Math.random() * 100);
-let countGuess = 10,
-    counter = 10;
+//Проверка на число
+const isNumber = function(num) {
+ return !isNaN(parseFloat(num)) && isFinite(num);
+};
 
-function guessNumber (num) {
-  let userNumber = prompt('Угадай число от 1 до 100');
-   
-  if (userNumber === null) {
-    alert('Игра окончена!!');
-    return;
-  } else if (userNumber > num && userNumber !== '' && countGuess > 0) {
-    countGuess -= 1;
-    alert('Загаданное число меньше' + ', ' + 'осталось попыток' + ' ' + countGuess);
-    
-    // guessNumber(num);
-  } else if (userNumber < num && userNumber !== '' && countGuess > 0) {
-    countGuess -= 1;
-    alert('Загаданное число больше' + ', ' + 'осталось попыток' + ' ' + countGuess);
-    
-    // guessNumber(num);
-  } else if (userNumber == num) {
-    confirm('Поздравляю, Вы угадали' + ' ' + 'за' + ' ' + (counter - countGuess) + ' ' + 'попыток' + ' ' + 'Хотите сыграть еще?');
-    
-    return; 
-    
-  } else if (countGuess === 0) {
-    confirm('Вы использовали все попытки! Хотите сыграть еще?)');
-    return;
-        
-  } else {
-    alert('Введи число');
-    guessNumber(num);
+//Игра в угадайку
+
+function gameBot()
+{
+ //Задание переменных
+ let attempts = 10;
+ let number = prompt(`Угадай число от 1 до 100. Количество попыток: ${attempts}`);
+ let hiddenNumber = Math.ceil(Math.random() * 100);
+
+  //Проверка условий
+  function answer()
+  {
+    //Если попытки закончились
+    if (attempts <= 1)
+    {
+      number = confirm("Попытки закончились, хочешь сыграть еще?");
+      if (number === false)
+        {
+        number = alert("Игра окончена");
+        }
+        else if (number == true)
+        {
+          gameBot();
+        }   
+    }
+    else
+    {
+      //Проверка введенного числа с загаднным числом
+      if (number === null)
+      {
+        number = alert("Игра окончена");
+      }
+      else if (!isNumber(number)) //Если введена фраза а не число
+      {
+        number = prompt("Введите число!");
+        answer();
+      }
+      else if (number == hiddenNumber)
+      {
+        number = confirm("Поздравляю, ты угадал! Хочещь сыграть еще?");
+        if (number === false)
+        {
+        number = alert("Игра окончена");
+        }
+        else if (number == true)
+        {
+          gameBot();
+        }        
+      }
+      else if (number > hiddenNumber)
+      {
+        attempts--;
+        number = prompt(`Загаданное число меньше! Попробуй еще раз! Осталось попыток: ${attempts}`);
+        answer();
+      }
+      else if (number < hiddenNumber) 
+      {
+        attempts--;
+        number = prompt(`Загаданное число больше! Попробуй еще раз! Осталось попыток: ${attempts}`);
+        answer();
+      }
+    }
   }
-    
-  guessNumber(num);
+  answer();
+
 }
-  
-guessNumber(botNumber);
+gameBot();
